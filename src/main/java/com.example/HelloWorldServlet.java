f68f63cee09eb7023afdf3175dd28f1ba9ea2d27
seemag1385 <140643568+seemag1385@users.noreply.github.com>
2023-08-02 06:23:10 -0400

    Update HelloWorldServlet.java

--- src/main/java/com.example/HelloWorldServlet.java
+++ src/main/java/com.example/HelloWorldServlet.java
@@ -12,6 +12,6 @@ public class HelloWorldServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html");
-        response.getWriter().println("<h1>Hello, World!</h1>");
+        response.getWriter().println("<h1>Hello Big  World!</h1>");
     }
 }
